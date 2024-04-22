82e7ceaa57707b1382ba4c606537f191ca1ba6db
JinyuCong <congtaotao2001@gmail.com>
2024-04-22 20:44:29 +0200

    modif

--- projet_enrichi/settings.py
+++ projet_enrichi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-@ep$7)(p@nml&x=5_!8$t&9*hzg&zsu@3sknqimq352fq3+ugh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
